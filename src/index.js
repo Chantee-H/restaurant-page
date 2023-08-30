65f632ee6cebfbfcadab862cb08a4c278cc0b192
Chantee-H <Chantee.hoarau123@gmail.com>
2023-08-30 08:43:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ initialLoad();
 import { menu } from "./menu";
 import {contact} from "./contact";
 
-const headBackground = document.getElementById('header');
-let h1 = document.createElement('h1');
-headBackground.appendChild(h1);
-h1.textContent = 'The Ice Cream Palace'
-
 function tabSwitch(){
     const homeBtn = document.querySelector('.home');
     homeBtn.addEventListener('click', initialLoad);
