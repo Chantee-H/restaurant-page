e36954c5917e90ea25208c83cdb0845ffbd7d7d4
Chantee-H <Chantee.hoarau123@gmail.com>
2023-08-30 09:23:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { initialLoad } from "./initial-load-page";
 initialLoad();
 
+let h1 = document.createElement('h1');
+main.appendChild(h1);
+footer.classList.add('h1');
+h1.textContent = 'The Ice Cream Palace';
+
 import { menu } from "./menu";
 import {contact} from "./contact";
 
